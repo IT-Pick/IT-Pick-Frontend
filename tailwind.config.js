40172b4e8c4cfe990e443f513683fc28b6a26f51
yskim6772 <yskim6772@gmail.com>
2024-07-02 21:48:19 +0900

    Feat: MyPage 구현

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,27 @@
 // }
 
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'pretendard': ['Pretendard', 'sans-serif']
+      },
+      colors: {
+        'black': '#1D2228',
+        'white': '#FFFFFF',
+        'gray1': '#EDF0F3',
+        'gray3': '#9EAAB5',
+        'point500': '#7620E4',
+        'point100': '#F1E6FF',
+        'errorpoint': '#FF4A4A'
+      },
+    },
   },
   plugins: [],
 }
