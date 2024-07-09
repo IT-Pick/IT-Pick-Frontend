ee933c4c8b434555373c61dd4d042ea6953f7d0d
clice lee <cliceleee@gmail.com>
2024-07-09 20:37:12 +0900

    fix: SearchBar margin 픽셀 조정

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,20 +4,15 @@ import IntegratedRanking from "./component/IntegratedRanking";
 import LiveDiscussion from "./component/LiveDiscussion";
 import CommunityRanking from "./component/CommunityRanking";
 
-const HomePage=()=>{
+const HomePage = () => {
   return (
-    <>
-    <div>
-    <Header/>
-      <div className="bg-white flex flex-col items-center w-screen"> {/*background color: white && applied to minimum height */}
-        <SearchBar/>
-        <IntegratedRanking/>
-        <LiveDiscussion/>
-        <CommunityRanking/>
-      </div>
+    <div className="bg-[#F8F9FC]">
+      <Header />
+      <SearchBar />
+      <IntegratedRanking />
+      <LiveDiscussion />
+      <CommunityRanking />
     </div>
-
-    </>
   );
 }
 
--- src/pages/HomePage/component/SearchBar.tsx
+++ src/pages/HomePage/component/SearchBar.tsx
@@ -4,9 +4,9 @@ import ico_itpick_logo from "../../../assets/images/ico_itpick_logo.svg";
 
 const SearchBar: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-start pt-10 px-4">
+    <div className="flex flex-col justify-start">
       <div className="mb-4 flex flex-col items-start">
-        <div className="text-[#2E333B] font-pretendard text-[20px] font-bold">
+        <div className="text-[#2E333B] font-pretendard text-[20px] font-bold ml-[24px]">
           <span className="block">현주님,</span>
           <span className="block">
             오늘의 <span className="text-[#7620E4]">연예</span> 소식을 확인해보세요!
@@ -14,12 +14,12 @@ const SearchBar: React.FC = () => {
         </div>
       </div>
 
-      <div className="relative mt-4 w-full max-w-md flex items-center">
-        <img src={ico_itpick_logo} alt="Logo Icon" className="w-8 h-8 mr-2" />
+      <div className="relative flex items-center mt-4 w-full max-w-md ml-[24px] mr-[24px]">
+        <img src={ico_itpick_logo} alt="Logo Icon" className="w-8 h-8 mr-[14.79px]" />
         <input 
           type="text" 
           placeholder="김현주 열애설" 
-          className="pl-4 pr-12 py-2 w-full rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-300" 
+          className="pl-4 pr-12 py-2 w-full rounded-full border border-transparent focus:outline-none focus:ring-2 focus:ring-gray-300" 
         />
         <span className="absolute right-4 top-1/2 transform -translate-y-1/2">
           <img src={ico_search} alt="Search Icon" className="w-5 h-5 text-gray-400" />
