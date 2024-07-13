66f9bda23d8d0ca587fbe024cd43efd85d318b82
clice lee <cliceleee@gmail.com>
2024-07-13 19:26:23 +0900

    add: 공통적으로 사용된 아이콘, 이미지 svg, png 파일로 추가 - 점진적으로 아이콘을  공통 폴더에 있는 것으로 수정해주세요

--- src/pages/HomePage/component/CommunityRanking.tsx
+++ src/pages/HomePage/component/CommunityRanking.tsx
@@ -5,29 +5,29 @@ import dcinside from '../../../assets/images/CommunityLogo/dcinside.svg';
 import naver from '../../../assets/images/CommunityLogo/naver.svg';
 import daumcafe from '../../../assets/images/CommunityLogo/daumcafe.svg';
 
-const CommunityRanking: React.FC = () => {
-  const rankings = [
-    { name: '나무위키', tag: '#김현주_열애설', icon: namuwiki },
-    { name: '트위터', tag: '#김현주_연애', icon: twitter },
-    { name: '디시인사이드', tag: '#김현주_남자친구', icon: dcinside },
-    { name: '네이버', tag: '#김현주_결혼', icon: naver },
-    { name: '다음카페', tag: '#김현주_공개연애', icon: daumcafe },
-  ];
+const rankings = [
+  { name: '나무위키', tag: '#김현주_열애설', icon: namuwiki },
+  { name: '트위터', tag: '#김현주_연애', icon: twitter },
+  { name: '디시인사이드', tag: '#김현주_남자친구', icon: dcinside },
+  { name: '네이버', tag: '#김현주_결혼', icon: naver },
+  { name: '다음카페', tag: '#김현주_공개연애', icon: daumcafe },
+];
 
+const CommunityRanking: React.FC = () => {
   return (
-    <div className="max-w-md mx-auto p-4 bg-white rounded-lg shadow-md">
-      <div className="flex justify-between items-center mb-4">
-        <h2 className="text-lg font-bold">커뮤니티 별 랭킹</h2>
-        <span className="text-gray-500">전체보기 &gt;</span>
+    <div className="items-start pt-10 px-4 ml-[24px]">
+      <div className="">
+        <span className="text-[#2E333B] font-pretendard text-[20px] font-bold leading-normal">통합 랭킹</span>
+        <span className="text-xs text-gray-500 ml-[8px]">4월 1일 18:00 기준</span>
       </div>
-      <p className="text-gray-500 mb-4">4월 1일 18:00 기준</p>
-      <ul>
+
+      <ul className="mt-4 space-y-4">
         {rankings.map((item, index) => (
           <li key={index} className="flex items-center mb-4">
             <img src={item.icon} alt={item.name} className="w-8 h-8 mr-4" />
             <div className="flex-1">
-              <p className="font-bold text-[#1D2228] font-pretendard text-[16px] leading-[22.4px]">{item.name}</p>
-              <p className="text-gray-500 text-[#1D2228] font-pretendard text-[16px] leading-[22.4px]">{item.tag}</p>
+              <span className="font-bold text-[#1D2228] font-pretendard text-[16px] leading-[22.4px]">{item.name}</span>
+              <span className="text-gray5 font-pretendard text-[16px] mr-[28px] ">{item.tag}</span>
             </div>
           </li>
         ))}
