af6337ecd7f04718772fdad0b990199f45cebfce
clice lee <cliceleee@gmail.com>
2024-07-23 19:31:02 +0900

    feat: 마이페이지, 회원가입 라우터 연결 -로그인 상태를 확인하고, false면 SignInPage로 연결 -true면 my-page로 연결

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,12 @@ import './index.css';
 import HomePage from './pages/HomePage/HomePage';
 import SignUpPage from './pages/SignUpPage/SignUpPage';
 import LoginPage from './pages/LoginPage/LoginPage';
-import ProfilePage from './pages/ProfilePage/ProfilePage';
+import SignInPage from './pages/SignInPage/SignInPage';
 import ParticipatedDebatesPage from './pages/HistoryPage/ParticipatedDebate';
 import DebatePage from './pages/HistoryPage/Debate';
 import RankingPage from './pages/RankingPage/RankingPage';
 import PwdChangePage from './pages/ChangeInfoPage/PwdChangePage';
-
+import MyPage from './pages/MyPage/MyPage';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
@@ -18,11 +18,13 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/signin" element={<SignInPage />} />
         <Route path="/participated-debates" element={<ParticipatedDebatesPage />} />
         <Route path="/debate" element={<DebatePage />} />
         <Route path="/ranking" element={<RankingPage />} />
         <Route path="/change-password" element={<PwdChangePage />} />
+        <Route path="/my-page" element={<MyPage />} />
+
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
--- src/pages/HomePage/component/Header.tsx
+++ src/pages/HomePage/component/Header.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import ico_alarm from "../../../assets/images/ico_alarm.svg";
 import ico_mypage from "../../../assets/images/ico_mypage.svg";
 
 const Header: React.FC = () => {
+  const navigate = useNavigate();
+  // 여기에 실제 로그인 상태를 확인하는 로직을 추가해야 함
+  const isLoggedIn = false; 
+  //const isLoggedIn = true;
+
+
+  const handleProfileClick = () => {
+    if (isLoggedIn) {
+      navigate('/signin');
+    } else {
+      navigate('/my-page');
+    }
+  };
+
   return (
     <header className="flex justify-end p-4">
       <div className="flex space-x-[32px]">
         <img src={ico_alarm} alt="Notifications" className="w-6 h-6" />
-        <img src={ico_mypage} alt="Profile" className="w-6 h-6 mr-[22px]" />
+        <img src={ico_mypage} alt="Profile" className="w-6 h-6 mr-[22px] cursor-pointer" onClick={handleProfileClick} />
       </div>
     </header>
   );
