30be0a6d7b62170b07ec2ccdc9dfef7bf04d4594
clice lee <cliceleee@gmail.com>
2024-07-23 19:07:26 +0900

    feat: 라우터 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import HomePage from './pages/HomePage/HomePage';
 import SignUpPage from './pages/SignUpPage/SignUpPage';
-import Login from './pages/LoginPage/LoginPage';
-import Profile from './pages/ProfilePage/ProfilePage';
-import ParticipatedDebates from './pages/HistoryPage/ParticipatedDebate';
-import Debate from './pages/HistoryPage/Debate';
+import LoginPage from './pages/LoginPage/LoginPage';
+import ProfilePage from './pages/ProfilePage/ProfilePage';
+import ParticipatedDebatesPage from './pages/HistoryPage/ParticipatedDebate';
+import DebatePage from './pages/HistoryPage/Debate';
 import RankingPage from './pages/RankingPage/RankingPage';
 import PwdChangePage from './pages/ChangeInfoPage/PwdChangePage';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-    <Profile/>
-    <Login/>
-    <Debate/>
-    <ParticipatedDebates/>
-    <HomePage/>
-    <RankingPage/>
-    <SignUpPage />
-    <PwdChangePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/participated-debates" element={<ParticipatedDebatesPage />} />
+        <Route path="/debate" element={<DebatePage />} />
+        <Route path="/ranking" element={<RankingPage />} />
+        <Route path="/change-password" element={<PwdChangePage />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
