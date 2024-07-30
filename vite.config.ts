0739e614a744b0cddfffa1873e77d8333a27d233
yskim6772 <yskim6772@gmail.com>
2024-07-30 23:33:24 +0900

    Chore: 불필요한 추가 코드 제거

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-  }
 })
