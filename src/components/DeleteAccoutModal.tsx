4fb0eb9e9c8753e220b1e60545d5e50de073d547
clice lee <cliceleee@gmail.com>
2024-08-01 00:53:55 +0900

    feat: 탈퇴하기 모달 기능 구현

--- src/components/DeleteAccoutModal.tsx
+++ src/components/DeleteAccoutModal.tsx
@@ -36,12 +36,10 @@ const DeleteAccoutModal: React.FC<DeleteAccoutModalProps> = ({ isOpen, onRequest
       className="flex flex-col items-center bg-[#ffffff] rounded-[24px] shadow-lg" 
     >
       <img src={trash} alt="trash_icon" className="w-[52px] h-[52px] mt-[20px]" />
-      <h2>
-        김잇픽
-      </h2>
-      <h2 className="text-[#1d2228] text-lg font-bold mt-[12px]">
-        님과 이별인가요..?
-      </h2>
+      <div className="text-[#1d2228] text-lg font-bold mt-[12px]">
+        <div className='inline-block'>김잇픽</div>
+        <div className='inline-block'>님과 이별인가요..?</div>
+      </div>
       <div className="w-[160px] h-[32px] text-[#464f59] text-center text-[14px] mt-2">
         <p>탈퇴 버튼 선택 시, 계정은</p>
         <p>삭제되며 복구되지 않습니다.</p>
