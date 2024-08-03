25f03531ed0a9d602635473f0687733b08a8b2d2
clice lee <cliceleee@gmail.com>
2024-08-03 21:16:01 +0900

    feat: my alarm 속성에 tag 추가 및 css 수정

--- src/pages/MyAlarmPage/MyAlarmPage.tsx
+++ src/pages/MyAlarmPage/MyAlarmPage.tsx
@@ -4,34 +4,42 @@ interface Alarm {
   id: number;
   title: string;
   about: string;
+  tag: string;
   time: number;
 }
 
 const Alarms: Alarm[] = [
-    { id: 1, title: '[김현주 열애설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요.",  time: 202406310001 },
-    { id: 2, title: '[김현주 열애설 어떻게 생각함?]', about: "토론이 인기글에 선정되었어요.", time: 202406310110 },
-    { id: 3, title: '[김현주 열애설 어떻게 생각함? 나는 일단 안 믿을래. 김현주는 우리 모두의 것이니까.]', about: "토론에 댓글이 달렸어요", time: 202407010001 },
-    { id: 4, title: '[잇픽 일등할 것 같은 사람 모여봐]', about: "토론에 댓글이 달렸어요", time: 202407110001 },
-    { id: 5, title: '[김현주 불화설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요", time: 202407210001 },
-    { id: 6, title: '[김현주 최고설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요", time: 202407250001 },
-    { id: 7, title: '[김현주 최고설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요", time: 202407270001 },
-    { id: 8, title: '[김현주 최고설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요", time: 202407310001 },
-    { id: 9, title: '[김현주 열애설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요", time: 20240801001 },
-    { id: 10, title: '[김현주 열애설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요", time: 202408011101 },
-    { id: 11, title: '[김현주 최고설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요", time: 202408011201 },
-    { id: 12, title: '[김현주 열애설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요", time: 202408011401 },
-    { id: 13, title: '[김현주 열애설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요", time: 202408020401 },
-    { id: 14, title: '[김현주 최고설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요", time: 202408020601 },
-    { id: 15, title: '[잇픽 최고설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요", time: 202408021201 },
-    { id: 16, title: '[김현주 열애설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요", time: 202408021501 },
+    { id: 1, title: '[김현주 열애설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요.", tag:"#김현주_열애설", time: 202406310001 },
+    { id: 2, title: '[김현주 열애설 어떻게 생각함?]', about: "토론이 인기글에 선정되었어요.", tag:"#김현주_열애설",time: 202406310110 },
+    { id: 3, title: '[김현주 열애설 어떻게 생각함? 나는 일단 안 믿을래. 김현주는 우리 모두의 것이니까.]',tag:"#김현주_열애설", about: "토론에 댓글이 달렸어요", time: 202407010001 },
+    { id: 4, title: '[잇픽 일등할 것 같은 사람 모여봐]', about: "토론에 댓글이 달렸어요",tag:"#김현주_열애설", time: 202407110001 },
+    { id: 5, title: '[김현주 불화설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요", tag:"#김현주_열애설",time: 202407210001 },
+    { id: 6, title: '[김현주 최고설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요", tag:"#김현주_열애설",time: 202407250001 },
+    { id: 7, title: '[김현주 최고설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요", tag:"#김현주_열애설",time: 202407270001 },
+    { id: 8, title: '[김현주 최고설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요", tag:"#김현주_열애설",time: 202407310001 },
+    { id: 9, title: '[김현주 열애설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요", tag:"#김현주_열애설",time: 20240801001 },
+    { id: 10, title: '[김현주 열애설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요", tag:"#김현주_열애설",time: 202408011101 },
+    { id: 11, title: '[김현주 최고설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요", tag:"#김현주_열애설",time: 202408011201 },
+    { id: 12, title: '[김현주 열애설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요", tag:"#김현주_열애설",time: 202408011401 },
+    { id: 13, title: '[김현주 열애설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요", tag:"#김현주_열애설",time: 202408020401 },
+    { id: 14, title: '[김현주 최고설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요",tag:"#김현주_열애설", time: 202408020601 },
+    { id: 15, title: '[잇픽 최고설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요",tag:"#김현주_열애설", time: 202408021201 },
+    { id: 16, title: '[김현주 열애설 어떻게 생각함?]', about: "토론에 댓글이 달렸어요", tag:"#김현주_열애설",time: 202408021501 },
 ];
 
-const AlarmItem: React.FC<Alarm> = ({ title, about, time }) => (
+const AlarmItem: React.FC<Alarm> = ({ title, about, tag,time }) => (
   <div className="bg-[#F3EEFF] p-4 mb-2 rounded-lg">
       <div className="flex justify-between">
-        <div className="text-[16px] font-bold">{title} {about}</div>
+        <div className="text-[14px] text-gray5 font-semibold">{title} {about}</div>
       </div>
-      <p className="text-[12px] text-gray-500">30분 전</p>
+      <p className='flex'>
+        <div className="text-[12px] text-gray3">
+          {time} | 
+        </div>
+        <div className='text-[12px] text-point500'>
+          {tag}
+        </div>
+      </p>
     
   </div>
 );
