00b1687dc5dae8aa87a9560744c617be05fd42ea
yskim6772 <yskim6772@gmail.com>
2024-08-04 20:41:21 +0900

    Chore: main.tsx 수정

--- src/main.tsx
+++ src/main.tsx
@@ -13,10 +13,10 @@ import PwdChangePage from './pages/ChangeInfoPage/PwdChangePage';
 import MyPage from './pages/MyPage/MyPage';
 import ProfileEditPage from './pages/ChangeInfoPage/ProfileEditPage';
 import NewSetProfile from './pages/SetProfile/NewSetProfile';
-import SearchPage from './pages/SearchPage/SearchPage';
 import AgreementPage from './pages/AgreementPage/AgreementPage';
 import NoDataPage from './components/NoDataPage';
 import KeywordPage from './pages/KeywordPage/KeywordPage';
+import SearchPage from './pages/SearchPage/SearchPage';
 import ErrorPage from './pages/ErrorPage/404ErrorPage';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -34,12 +34,12 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path="/change-password" element={<PwdChangePage />} />
         <Route path="/my-page" element={<MyPage />} />
         <Route path="/profile-edit" element={<ProfileEditPage />} />
-        <Route path="/search" element={<SearchPage />} />
         <Route path='/agreement' element={<AgreementPage/>} />
         <Route path='/debate-no-data' element={<NoDataPage id={1}/>} />
         <Route path='/participated-debate-no-data' element={<NoDataPage id={2}/>} />
         <Route path='/notification-no-data' element={<NoDataPage id={3}/>} />
         <Route path='/keyword' element={<KeywordPage/>} />
+        <Route path="/search" element={<SearchPage />} />
         <Route path='*' element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
