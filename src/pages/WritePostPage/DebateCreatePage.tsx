0d4ca454d37705c969c3e823cf4488e7b98c9937
yskim6772 <yskim6772@gmail.com>
2024-08-07 14:46:21 +0900

    Chore: css 수정

--- src/pages/WritePostPage/DebateCreatePage.tsx
+++ src/pages/WritePostPage/DebateCreatePage.tsx
@@ -6,9 +6,9 @@ const DebateCreatePage: React.FC = () => {
   const [content, setContent] = useState('');
 
   return (
-    <div className="w-[390px] h-full mx-auto flex flex-col items-center justify-between">
-      <div className="bg-background w-full p-6">
-      <div className="flex justify-between items-center mb-10">
+    <div className="w-[390px] h-screen mx-auto flex flex-col items-center justify-between bg-background">
+      <div className="w-full">
+        <div className="flex justify-between items-center mb-10 py-4 px-6 bg-white">
           <div className="font-pretendard font-bold text-lg">
             <span className="text-point500">#김현주 열애설</span>
             <span className="text-black"> 토론 만들기</span>
@@ -20,17 +20,17 @@ const DebateCreatePage: React.FC = () => {
           placeholder="제목을 입력해주세요."
           value={title}
           onChange={(e) => setTitle(e.target.value)}
-          className="w-[335px] px-3 font-pretendard font-bold text-[18px] text-gray3 border-none focus:outline-none bg-background"
+          className="w-[335px] px-5 font-pretendard font-bold text-[18px] text-gray3 border-none focus:outline-none bg-background"
         />
         <hr className="w-[350px] border-t border-gray1 my-[20px]" />
         <textarea
           placeholder="자유롭게 이야기해 보세요."
           value={content}
           onChange={(e) => setContent(e.target.value)}
-          className="w-[335px] h-[109px] px-3 font-pretendard font-medium text-[16px] text-gray3 border-none focus:outline-none resize-none bg-background"
+          className="w-[335px] h-[109px] px-5 font-pretendard font-medium text-[16px] text-gray3 border-none focus:outline-none resize-none bg-background"
         />
       </div>
-      <div className="w-[390px] flex justify-center mt-5 mb-5">
+      <div className="w-[390px] flex justify-center py-3 bg-white">
         <DebateIconBar />
       </div>
     </div>
