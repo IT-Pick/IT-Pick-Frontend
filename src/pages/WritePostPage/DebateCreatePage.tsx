a3571366045c22a3499812aa38bfd43117c5452e
yskim6772 <yskim6772@gmail.com>
2024-08-08 20:11:11 +0900

    Chore: 내용 입력란 크기 조정

--- src/pages/WritePostPage/DebateCreatePage.tsx
+++ src/pages/WritePostPage/DebateCreatePage.tsx
@@ -22,7 +22,7 @@ const DebateCreatePage: React.FC = () => {
 
   return (
     <div className="w-[390px] h-screen mx-auto flex flex-col items-center justify-between bg-background">
-      <div className="w-full">
+      <div className="w-full h-full flex flex-col">
         <div className="flex justify-between items-center mb-6 py-4 px-6 bg-white">
           <div className="font-pretendard font-bold text-lg">
             <span className="text-point500">#김현주 열애설</span>
@@ -42,7 +42,7 @@ const DebateCreatePage: React.FC = () => {
           placeholder="자유롭게 이야기해 보세요."
           value={content}
           onChange={(e) => setContent(e.target.value)}
-          className="w-[335px] h-full px-5 font-pretendard font-medium text-[16px] text-gray3 border-none focus:outline-none resize-none bg-background"
+          className="w-[335px] flex-grow px-5 font-pretendard font-medium text-[16px] text-gray3 border-none focus:outline-none resize-none bg-background"
         />
       </div>
       <div className={`w-[390px] flex justify-center py-3 bg-white ${isKeyboardVisible ? 'fixed bottom-0' : ''}`}>
