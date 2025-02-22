0d46c736915f39825e5ed13c077384ff9eb9bafc
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2025-02-22 22:18:37 +0900

    Chore: Delete shadcn chart config in tailwind.config.js
    -delete unused code

--- tailwind.config.js
+++ tailwind.config.js
@@ -71,13 +71,13 @@ module.exports = {
   			border: 'hsl(var(--border))',
   			input: 'hsl(var(--input))',
   			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
+  			// chart: {
+  			// 	'1': 'hsl(var(--chart-1))',
+  			// 	'2': 'hsl(var(--chart-2))',
+  			// 	'3': 'hsl(var(--chart-3))',
+  			// 	'4': 'hsl(var(--chart-4))',
+  			// 	'5': 'hsl(var(--chart-5))'
+  			// }
   		},
   		maxWidth: {
   			custom: '680px'
